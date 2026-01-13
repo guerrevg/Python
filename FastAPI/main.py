2def3f484cdf06e8fc1560c4695a6543818aae0b
Pritam <hackdartstorm@gmail.com>
2026-01-12 22:41:04 -0800

    Day 21

---
+++ FastAPI/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, HTTPException
+from typing import Any
+from datetime import datetime
+
+app = FastAPI(root_path="/api/v1")
+
+@app.get('/')
+async def root():
+    return {"message":"Hello World !"}
+
+data : Any = [
+    {
+        "campaogn_id":1,
+        "name":"Summer Launch",
+        "due_date" : datetime.now(),
+        "created_at" : datetime.now()
+    },
+    {
+        "campaogn_id":2,
+        "name":"Black Friday",
+        "due_date" : datetime.now(),
+        "created_at" : datetime.now()
+    }
+]
+
+
+@app.get("/campaigns")
+async def read_campaigns():
+    return {"Campaigns",data}
+
+@app.get("/campaigns/{id}")
+async def read_campaign(id:int):
+    for campaign in data:
+        if campaign.get("campaign_id") == id:
+            return {"campaign":campaign}
+    raise HTTPException(status_code=404)
+
+
+
+#------------------------------------------Work-In-Progress---------------------------------------------------------

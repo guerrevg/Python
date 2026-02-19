1bc0def1e22f88f92a4f3967b0e19aa87eb293eb
Guilherme Kauan <guilhermekauamt01@gmail.com>
2026-02-19 10:17:25 -0400

    Solve problem 1880: word equals summation

---
+++ exercises/1000_programs/medium/1880_word_sum.py
@@ -0,0 +1,16 @@
+def word_to_number(word: str) -> int:
+    num_str = ""
+    for ch in word:
+        num_str += str(ord(ch) - ord("a"))
+    return int(num_str)
+
+
+def is_sum_equal(firstWord: str, secondWord: str, targetWord: str) -> bool:
+    return word_to_number(firstWord) + word_to_number(secondWord) == word_to_number(targetWord)
+
+
+if __name__ == "__main__":
+    # Exemplos básicos para teste manual
+    print(is_sum_equal("acb", "cba", "cdb"))  # True
+    print(is_sum_equal("aaa", "a", "aaaa"))   # True
+    print(is_sum_equal("a", "b", "c"))        # False
